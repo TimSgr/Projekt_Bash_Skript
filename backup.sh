eaa17d72208ea2d9a2417bec1aa1664a3c1f3fc5
Tim Seeger <tim.seeger@stackfuel.com>
2019-11-19 16:38:46 +0100

    Finales Ergebnis

--- backup.sh
+++ backup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+set -e
 ###Schritt 1  Überprüfen ob Eingaben richtig sind
 if [ "$#" -ne 2 ]; then
     echo $#
@@ -24,15 +24,19 @@ else
 ###Schritt 3 Neue Datei wird einer Variable zugewiesen und Existens des Ordners wird überprüft
     Neue_Datei=$FILE-$(date '+%d-%m-%y')$EXTN
 
-if [ ! -d $2 ]; then
-  echo "Der Ordner $2 existiert nicht"
-  sleep 2
-###Schritt 4 Ordner wird erstellt wenn er noch nicht vorhanden ist und die DAtei wird dahin verschoben
-  echo "Es wird ein neuer Ordner mit dem Namen $2 erstellt..."
-  mkdir -p $2;
-  echo "Der Ordner wurde erstellt und die Datei wurde gespeichert"
-    else echo "Die Datei wird im Ordner $2 gespeichert..."
-mv $Neue_Datei /$2
+if [[ $2 == *['!''?''='@\$%^\&*()_+]* ]]
+then echo "Unzulässiger Ordnername";
+else
 
+    if [ ! -d $2 ]; then
+    echo "Der Ordner $2 existiert nicht"
+    sleep 2
+    ###Schritt 4 Ordner wird erstellt wenn er noch nicht vorhanden ist und die DAtei wird dahin verschoben
+    echo "Es wird ein neuer Ordner mit dem Namen $2 erstellt..."
+    mkdir -p $2;
+    echo "Der Ordner wurde erstellt und die Datei wurde gespeichert"
+    else echo "Der Ordner $2 existiert und die Datei wurde darin gespeichert..."
+    mv $Neue_Datei ./$2
+fi
 fi
 fi
