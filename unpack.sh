91876094579991a85dc201e082ca94418cf97b68
Tim Seeger <tim.seeger@stackfuel.com>
2019-11-22 10:33:26 +0100

    erneut überarbeitet

--- unpack.sh
+++ unpack.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
-if [ "$#" -lt 2 ]; then
+
+if [ "$#" -lt 2 ] 
+then
     echo "Bitte geben Sie eine Datei an von der ein Backup erstellt wurde und einen
     Speicherort für die neue Datei ";
     exit 1
 else
 ZIEL_ORDNER=$2
 PFAD=$1
+
 ORDNER=`dirname "$1"`
 DATEI=`basename "$1"` 
-#echo "$DATEI"
-# echo "$ORDNER"
+    
     if [ -d "$ORDNER" ]
     then    
         cd $ORDNER
@@ -17,26 +19,35 @@ DATEI=`basename "$1"`
         then  
             echo "Datei wird gesucht..."
             echo "Datei wird versucht zu entpacken..."
-            #echo "$DATEI"
             mkdir -p $ZIEL_ORDNER   
-            mv $DATEI $ZIEL_ORDNER
-            # cd $ZIEL_ORDNER
-            # mv $DATEI C /
+            mv $DATEI $ZIEL_ORDNER 2>/dev/null
             cd $ZIEL_ORDNER
-            tar -xzf $DATEI
-            echo "Dateien erfolgreich komprimiert in den Ordner $ZIEL_ORDNER"
-           # mv $DATEI $ZIEL_ORDNER
+            tar -xzf $DATEI 2>/dev/null
+            if [ $ZIEL_ORDNER == "." ]
+            then
+                echo "Dateien erfolgreich komprimiert in den Ordner $ORDNER"
+            else    
+                echo "Dateien erfolgreich komprimiert in den Ordner $ZIEL_ORDNER"
+            fi
         else 
-            echo "Datei nicht gefunden"
+            cd $ZIEL_ORDNER
+            ls
+            echo $DATEI
+            echo "Datei nicht gefunden"    
         fi
     else 
-        echo $ORDNER
-        echo "Ordner nicht gefunden"
-        echo "Ordner wird erstellt"
-        cd $ZIEL_ORDNER
-        mkdir -p $ZIEL_ORDNER 
-        #mv $DATEI ~/$ZIEL_ORDNER
-
+        if [ $ZIEL_ORDNER == "." ]
+        then
+            cd $ORDNER
+            tar -xzf $DATEI 2>/dev/null
+        else
+            echo $ZIEL_ORDNER
+            echo "Ordner nicht gefunden"
+            echo "Ordner wird erstellt"
+            mkdir -p $ZIEL_ORDNER 
+            cd $ZIEL_ORDNER
+            tar -xzf $DATEI 2>/dev/null
+        fi
     fi
 fi
 find . -empty -type d -delete
